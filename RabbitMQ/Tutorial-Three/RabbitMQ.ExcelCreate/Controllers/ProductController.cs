1e54960e1e7a74fbbeac1f02933390b84807b1de
Engin Yenice <enginyenice2626@gmail.com>
2021-06-19 15:57:39 +0300

    ProductController > CreateProductExcel() ve Files()

--- RabbitMQ/Tutorial-Three/RabbitMQ.ExcelCreate/Controllers/ProductController.cs
+++ RabbitMQ/Tutorial-Three/RabbitMQ.ExcelCreate/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.ExcelCreate.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +13,38 @@ namespace RabbitMQ.ExcelCreate.Controllers
     [Authorize]
     public class ProductController : Controller
     {
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProductController(AppDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CreateProductExcel()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var fileName = $"product-excel-{Guid.NewGuid().ToString().Substring(1, 10)}";
+            UserFile userFile = new UserFile { UserId = user.Id, FileName = fileName, FileStatus = FileStatus.Creating };
+            await _context.UserFiles.AddAsync(userFile);
+            await _context.SaveChangesAsync();
+            //rabbitMQ mesaj gönder
+            TempData["StartCreatingExcel"] = true;
+            return RedirectToAction(nameof(Files));
+        }
+
+        public async Task<IActionResult> Files()
+        {
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return View(await _context.UserFiles.Where(p => p.UserId == user.Id).ToListAsync());
         }
     }
 }
