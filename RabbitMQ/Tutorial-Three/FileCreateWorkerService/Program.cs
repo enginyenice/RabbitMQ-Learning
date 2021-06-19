e15d36fe82890792d6f6d8e4cb62128690a2431d
Engin Yenice <enginyenice2626@gmail.com>
2021-06-19 16:53:35 +0300

    WorkerService

---
+++ RabbitMQ/Tutorial-Three/FileCreateWorkerService/Program.cs
@@ -0,0 +1,33 @@
+﻿using FileCreateWorkerService.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileCreateWorkerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    IConfiguration Configuration = hostContext.Configuration;
+                    services.AddSingleton<RabbitMQClientService>();
+                    //appsettings.json dosyasından rabbitmq bağlantı cümlemizi aldık ve singleton olarak oluşturduk.
+                    //Buradan 1 adet esne örneği oluşturulacak
+                    services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")), DispatchConsumersAsync = true });
+                    
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
