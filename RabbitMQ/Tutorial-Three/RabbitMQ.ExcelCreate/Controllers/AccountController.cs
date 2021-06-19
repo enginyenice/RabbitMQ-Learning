8a7fa5fc10e6db4f8b2dfa492fd0ecbb9d28e02f
Engin Yenice <enginyenice2626@gmail.com>
2021-06-19 15:33:59 +0300

    AccountController and Login Page

---
+++ RabbitMQ/Tutorial-Three/RabbitMQ.ExcelCreate/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.ExcelCreate.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email,string Password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(Email);
+            if (hasUser == null)
+            {
+                return View();
+            }
+
+            var signResult = await _signInManager.PasswordSignInAsync(hasUser, Password,true,false);
+            if(!signResult.Succeeded)
+            {
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
