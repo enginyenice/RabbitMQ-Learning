8b7f5b811005ced1eb216c31eb288be3bfc1c46f
Engin Yenice <enginyenice2626@gmail.com>
2021-06-20 11:58:18 +0300

    Tutorial-Four

---
+++ RabbitMQ/Tutorial-Four/RabbitMQ.Word_To_Pdf_Producer/Models/WordToPdf.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Word_To_Pdf_Producer.Models
+{
+    public class WordToPdf
+    {
+        public string Email { get; set; }
+        public IFormFile WordFile{ get; set; }
+    }
+}
